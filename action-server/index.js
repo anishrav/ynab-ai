8bf061d80e45ba8709650b570cc2835b34cc6cca
anishrav <anishrav@umich.edu>
2019-05-14 00:17:46 -0400

    configured budget retrieval + listing out transactions; next step is to make output variable with entities/slots

--- action-server/index.js
+++ action-server/index.js
@@ -11,14 +11,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/webhook', (req, res) => handleMessage(req, res));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => { 
+    console.log(`Example app listening on port ${port}!`);
+});
 
 function handleMessage(req, res) {
     let incoming_request = req.body;
     console.log(incoming_request['tracker']['latest_message']['entities']);
     console.log(incoming_request['tracker']['slots']);
-    console.log('');
     if (incoming_request['next_action'] === "action_activity") {
+        let budget_id = '';
+        const budgetsResponse = ynabAPI.budgets
+            .getBudgets()
+            .then(res => {
+                const budgets = res.data.budgets;
+                budget_id = budgets[0]['id'];
+            })
+            .then( () => {
+                console.log(ynabAPI.transactions
+                    .getTransactions(budget_id, '2019-04-30')
+                    .then(res => console.log(res.data.transactions)));
+            })
+            .catch(e => {
+                console.log(e);
+            });
+
         // handle the activity reponse
         let response = {};
         response['responses'] = [];
