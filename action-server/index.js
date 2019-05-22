70251cb884d39bc5601e80d9d3848d399ca16d6c
anishrav <anishrav@umich.edu>
2019-05-21 20:31:35 -0400

    spent_timeframe action

--- action-server/index.js
+++ action-server/index.js
@@ -114,4 +114,120 @@ function handleMessage(req, res) {
         })
         .catch( e => console.error(e));
     }
+    if (incoming_request['next_action'] === "action_spent_timeframe") {
+        // get start date and end date
+        // pull budget_action, time_count and time_unit from slots
+        let start_date = incoming_request['tracker']['slots']['start_date'];
+        let end_date = incoming_request['tracker']['slots']['end_date'];
+        let time_unit = incoming_request['tracker']['slots']['time_unit'];
+
+        let start = new Date(start_date);
+        let end = new Date(end_date);
+        let today = new Date();
+
+        // if year on start/end date is 2001, change based on time_unit slot
+        if (start.getYear() - 100 === 1) {
+            if (time_unit === 'year') {
+                start.setYear(today.getYear() - 1);
+            }
+        }
+
+        if (end.getYear() - 100 === 1) {
+            if (time_unit === 'year') {
+                end.setYear(today.getYear() - 1);
+            }
+        }
+
+        // fetch total transactions from start_date to today
+
+        // pull transactions for timeframe
+        let api_url = url.format({
+            protocol: 'https',
+            hostname: `api.youneedabudget.com`,
+            pathname: `v1/budgets/${budget_id}/transactions`,
+            query: {
+                since_date: start.toISOString().split('T')[0]
+            }
+        });
+
+        let start_amount = fetch(api_url, {
+            method: 'get',
+            headers: new Headers({
+                'Authorization': 'Bearer ' + accessToken
+            }),
+        })
+        .then( response => response.json() )
+        .then( json => {
+            let transactions = json.data.transactions;
+
+            // calculate total amount of outflow
+            let amount = 0;
+            for (let transaction of transactions) {
+                // console.log(transaction.payee_name);
+                if (transaction.amount < 0 
+                    && transaction.cleared == 'cleared' 
+                    && !transaction.transfer_account_id) {
+                    amount += transaction.amount;
+                }
+            }
+            
+            return amount / 1000 * -1;
+        })
+        .catch( e => console.error(e));
+
+        // fetch total transactions from end_date to today
+
+        // pull transactions for timeframe
+        let api_url = url.format({
+            protocol: 'https',
+            hostname: `api.youneedabudget.com`,
+            pathname: `v1/budgets/${budget_id}/transactions`,
+            query: {
+                since_date: end.toISOString().split('T')[0]
+            }
+        });
+
+        let end_amount = fetch(api_url, {
+            method: 'get',
+            headers: new Headers({
+                'Authorization': 'Bearer ' + accessToken
+            }),
+        })
+        .then( response => response.json() )
+        .then( json => {
+            let transactions = json.data.transactions;
+
+            // calculate total amount of outflow
+            let amount = 0;
+            for (let transaction of transactions) {
+                // console.log(transaction.payee_name);
+                if (transaction.amount < 0 
+                    && transaction.cleared == 'cleared' 
+                    && !transaction.transfer_account_id) {
+                    amount += transaction.amount;
+                }
+            }
+            
+            return amount / 1000 * -1;
+        })
+        .catch( e => console.error(e));
+
+        Promise.all([start_amount, end_amount]).then(
+            (start, end) => {
+                // once both promises are fulfilled, subtract start_amount from end_amount
+                return end - start;
+            }
+        )
+        .then( amount => {
+            // handle the reponse to user
+            let response = {};
+            response['responses'] = [];
+            let text = `Between ${start_date} and ${end_date}, you spent $${amount}.`;
+            response['responses'].push({'text': text});
+            res.json(response);
+            return;
+        })
+        .catch(e => console.log(e));
+
+    }
 }
